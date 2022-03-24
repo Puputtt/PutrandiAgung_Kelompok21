c8cb6885d1f569b6e524c27be2ef1970e4d16833
YosuaEvan <102003747+YosuaEvan@users.noreply.github.com>
2022-03-24 15:06:37 +0700

    Update PesanWarteg.py

--- Modul 4/PesanWarteg.py
+++ Modul 4/PesanWarteg.py
@@ -8,30 +8,47 @@
 # Kelompok: 21
 # Shift: 2
 # ==================================
+from threading import local
+from method_modul4 import *
 
 def tidak_ada_uang():
-    print("Maaf kamu tidak memiliki uang yang cukup")
+    return("Maaf kamu tidak memiliki uang yang cukup")
+
+def sisa_uang():
+    return("sisa uang mu adalah Rp." + str(uang))
 
 print("----------- Warteg'21 -----------")
 
 uang = int (input('Masukan jumlah uang anda : '))
 makanan = ["Rendang",16000], ["Ayam Bakar",15000], ["Ayam Goreng", 15000], ["Nasi Goreng", 13000]
 minuman = ["Teh Manis", 6000], ["Air Mineral", 3000], ["Teh Tawar", 5000], ["Jus Jeruk", 9000]
-tambahan = ['pergedel', 2000], ['kerupuk', 1000]
-bungkus = ['bawa pulang'], ['makan di tempat']
-pesan_makanan = True
-pesan_minuman = True
-pesan_tambahan = True
-pesan_bungkus = True
+tambahan = ['Pergedel', 2000], ['Kerupuk', 1000]
+pesan_makanan = False
+pesan_minuman = False
+pesan_tambahan = False
+jenis_pesan = True
+pesanan1 = False
+pesanan2 = False
+pesanan3 = False
 
 makananlen = len(makanan)
 minumanlen = len(minuman)
 tambahanlen = len(tambahan)
-bungkuslen = len(bungkus)
+
+pesan = Pesanan()
 
 #Print List Makanan
+print("---------------------------------")
 for i in range (makananlen):
     print(str(i+1) + ". " + makanan[i][0] + " (Rp." + str(makanan[i][1]) + ")")
+    
+#Mengecek apabila cukup uang untuk membeli menu    
+for j in range(makananlen):
+    if(uang >= makanan[j][1]):
+        pesan_makanan = True
+
+if(pesan_makanan == False):
+    print("\n" + tidak_ada_uang() + " untuk membeli salah satu menu makanan")
 
 while (pesan_makanan == True):
     choice1 = int(input("Pilih menu makanan yang diinginkan: "))
@@ -43,14 +60,26 @@ while (pesan_makanan == True):
             print("Maaf warteg kami tidak menyediakan porsi sebanyak itu")
         elif(uang >= (totalchoice1 * makanan[choice1-1][1])):
             uang = uang - (totalchoice1 * makanan[choice1-1][1])
-            print("Kamu telah membeli menu " + makanan[choice1-1][0] + " sebanyak " + str(totalchoice1) + " porsi dan kamu memiliki sisa uang Rp." + str(uang))
+            print("Kamu telah membeli menu " + makanan[choice1-1][0] + " sebanyak " + str(totalchoice1) + " porsi dan " + sisa_uang())
+            pesan.tambah_pesanan(makanan[choice1-1][0])
+            pesan.tambah_total(totalchoice1)
+            pesanan1 = True
             pesan_makanan = False
         else:
-            tidak_ada_uang()
+            print(tidak_ada_uang() + " coba pesan menu lain")
 
 #Print List Minuman
+print("---------------------------------")
 for i in range (minumanlen):
     print(str(i+1) + ". " + minuman[i][0] + " (Rp." + str(minuman[i][1]) + ")")
+   
+#Mengecek apabila cukup uang untuk membeli menu
+for j in range(minumanlen):
+    if(uang >= minuman[j][1]):
+        pesan_minuman = True
+
+if(pesan_minuman == False):
+    print("\n" + tidak_ada_uang() + " untuk membeli salah satu menu minuman")
 
 while (pesan_minuman == True):
     choice2 = int(input("Pilih menu minuman yang diinginkan: "))
@@ -62,16 +91,26 @@ while (pesan_minuman == True):
             print("Mohon maaf warteg kami tidak mempunyai minuman sebanyak itu")
         elif(uang >= (totalchoice2 * minuman[choice2-1][1])):
             uang = uang - (totalchoice2 * minuman[choice2-1][1])
-            print("Kamu telah membeli menu " + minuman[choice2-1][0] + " sebanyak " + str(totalchoice2) + " buah dan kamu memiliki sisa uang Rp." + str(uang))
+            print("Kamu telah membeli menu " + minuman[choice2-1][0] + " sebanyak " + str(totalchoice2) + " buah dan " + sisa_uang())
             pesan_minuman = False
+            pesan.tambah_pesanan(minuman[choice2-1][0])
+            pesan.tambah_total(totalchoice2)
+            pesanan2 = True
         else:
-           tidak_ada_uang()
+           print(tidak_ada_uang() + " coba pesan menu lain")
 
-
-
-#print tambahan
+#Print list Tambahan
+print("---------------------------------")
 for i in range (tambahanlen):
     print(str(i+1) + ". " + tambahan[i][0] + " (Rp." + str(tambahan[i][1]) + ")")
+
+#Mengecek apabila cukup uang untuk membeli menu
+for j in range(tambahanlen):
+    if(uang >= tambahan[j][1]):
+        pesan_tambahan = True
+
+if(pesan_tambahan == False):
+    print("\n" + tidak_ada_uang() + " untuk membeli salah satu menu tambahan")
 
 while (pesan_tambahan == True):
     choice3 = int(input("Pilih menu tambahan yang diinginkan: "))
@@ -85,22 +124,34 @@ while (pesan_tambahan == True):
             uang = uang - (totalchoice3 * tambahan[choice3-1][1])
             print("Kamu telah membeli menu " + tambahan[choice3-1][0] + " sebanyak " + str(totalchoice3) + " buah dan kamu memiliki sisa uang Rp." + str(uang))
             pesan_tambahan = False
+            pesan.tambah_pesanan(tambahan[choice3-1][0])
+            pesan.tambah_total(totalchoice3)
+            pesanan3 = True
         else:
-           tidak_ada_uang()
+            print(tidak_ada_uang() + " coba pesan menu lain")
 
-#makan di temppat atau bawa pulang
-for i in range (bungkuslen):
-    print(str(i+1) + ". " + bungkus[i][0])
+# Ingin dibawa pulang / makan di tempat
+print("---------------------------------")
+if(pesanan1 == True or pesanan2 == True or pesanan3 == True):
+    print("1. Dibungkus \n2. Makan di tempat")
+    while(jenis_pesan == True):
+        choice4 = int(input("Ingin dibungkus atau makan di tempat?"))
+        if(choice4 == 1):
+            bungkus = True
+            jenis_pesan = False
+        elif(choice4 == 2):
+            bungkus = False
+            jenis_pesan = False
+        else:
+            print("Mohon masukkan format yang benar")
 
-
-while (pesan_bungkus == True):
-    choice4 = int(input("makan di tempat atau bungkus? : "))
-    if(choice4 == 1):
-        print("\nKamu telah memesan " + makanan[choice1-1][0] + " " + str(totalchoice1) + " porsi, " + minuman[choice2-1][0] + " " + str(totalchoice2) + " " +" " + tambahan[choice3-1][0] + ' '+ str(totalchoice3) +" " + "  buah dibungkus")
-        print('mohon ditunggu ')
-        pesan_bungkus = False
+    print("\nBerikut adalah pesananmu: ")
+    pesan.total_pesanan()
+    if(bungkus == True):
+        print("\nMohon tunggu sebentar, kami akan membungkus pesananmu")
     else:
-        print("\nKamu telah memesan " + makanan[choice1-1][0] + " " + str(totalchoice1) + " porsi, " + minuman[choice2-1][0] + " " + str(totalchoice2) + " " + tambahan[choice3-1][0] + ' '+ str(totalchoice3) +" " + "buah makan di tempat")
-        pesan_bungkus = False
+        print("\nMohon tunggu sebentar.... \nSelamat Menikmati!")
 
-print ('selamat menikmati')
+    print(sisa_uang())
+else:
+    print("Kamu tidak memesan apapun")
